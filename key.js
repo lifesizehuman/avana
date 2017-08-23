f7e00958de68132a8099b6f98bc6431fbf57114a
Slater Combes <combes.slater@gmail.com>
2017-08-23 08:04:40 -0700

    load twitter api keys

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'Y2PazwBmci5i7z2Q3oP5NyMhi',
+  consumer_secret: '24u8jgGYNNrrOKPP0POrzVG74z7HNM8R5DpHsWeDOzXggdYmi1',
+  access_token_key: '3158314694-fhIxWIsEn4KIigHKAWG0AHDGW33MvcGhtbwzJbo',
+  access_token_secret: '	liXOA5eJvvz9xNvJAhmTaiuYRV53WLyk3fRYEiPKYGJU1',
 }
