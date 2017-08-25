b3936a0844bc464373f43b0defce4ec822143555
Slater Combes <combes.slater@gmail.com>
2017-08-24 17:39:21 -0700

    fix api keys

--- avana.js
+++ avana.js
@@ -1,8 +1,8 @@
 var keys = require("./key.js");
 
-
-
 var request = require("request");
+
+// var command = process.argv[2];
 
 var command = "";
 
@@ -10,26 +10,39 @@ for (var i = 2; i < process.argv.length; i++) {
   command += process.argv[i] + " ";
 }
 
-var twit = require('twitter');
+
 
 function getTweets() {
-  if (command === "get tweets") {
 
-    twitter = require('twitter');
-    var twit = twitter({
+    var twitter = require('twitter');
+
+    var params = {
+    screenName: "OCMemeDad"
+    }
+
+    var twit = new twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
   });
 
-  twit.get("http://api.twitter.com/1.1/statuses/user_timeline.json?count=2",
-   function(error, response, body){
-     if (!error && response.statusCode === 200) {
-       console.log(response);
-    }
-  })
- }
-}
+  console.log(keys.twitterKeys);
+
+
+  twit.get("statuses/user_timeline", params,
+   function(error, tweets, response){
+     console.log(error);
+     if (!error) {
+       for(var i = 0; i < tweets.length; i++) {
+         console.log(tweets[i].text);
+       }
+     }
+   })
+  }
+
+if (command === "getTweets") {
 
 getTweets();
+
+}
--- key.js
+++ key.js
@@ -4,5 +4,5 @@ exports.twitterKeys = {
   consumer_key: 'Y2PazwBmci5i7z2Q3oP5NyMhi',
   consumer_secret: '24u8jgGYNNrrOKPP0POrzVG74z7HNM8R5DpHsWeDOzXggdYmi1',
   access_token_key: '3158314694-fhIxWIsEn4KIigHKAWG0AHDGW33MvcGhtbwzJbo',
-  access_token_secret: '	liXOA5eJvvz9xNvJAhmTaiuYRV53WLyk3fRYEiPKYGJU1',
+  access_token_secret: 'liXOA5eJvvz9xNvJAhmTaiuYRV53WLyk3fRYEiPKYGJU1',
 }
