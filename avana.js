079abd7d25252924e125f67d27396430ec9918de
Slater Combes <combes.slater@gmail.com>
2017-08-29 15:47:46 -0700

    double quotes on strings

--- avana.js
+++ avana.js
@@ -1,12 +1,12 @@
-var keys = require('./key.js');
-var request = require('request');
+var keys = require("./key.js");
+var request = require("request");
 var command = process.argv[2];
 
 function getTweets() {
   var input = process.argv[3];
 
-  var Twitter = require('twitter');
-  var searchURL = 'https://api.twitter.com/1.1/users/search.json?q=' + input + '&page=1&count=3';
+  var Twitter = require("twitter");
+  var searchURL = "https://api.twitter.com/1.1/users/search.json?q=" + input + "&page=1&count=3";
 
   var twit = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
@@ -23,21 +23,21 @@ function getTweets() {
       var friends = tweets[0].friends_count;
       var tweetCount = tweets[0].statuses_count;
 
-      var twitterURL = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + handle + '&limit=20';
+      var twitterURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + handle + "&limit=20";
 
       twit.get(twitterURL, function(error, tweets, response) {
         var stats = [
-          'The Tweets of ' + realName,
-          '----------',
-          'User Stats',
-          '----------',
-          'Username: ' + handle,
-          'Total tweets: ' + tweetCount,
-          'Followers: ' + followers,
-          'Following: ' + friends,
-          '----------',
-          'Most recent tweets',
-          '----------'
+          "The Tweets of " + realName,
+          "----------",
+          "User Stats",
+          "----------",
+          "Username: " + handle,
+          "Total tweets: " + tweetCount,
+          "Followers: " + followers,
+          "Following: " + friends,
+          "----------",
+          "Most recent tweets",
+          "----------"
         ];
         for (var i = 0; i < stats.length; i++) {
           console.log(stats[i]);
@@ -48,7 +48,7 @@ function getTweets() {
             var creation = tweets[j].created_at;
             console.log(tweet);
             console.log(creation);
-            console.log('----------');
+            console.log("----------");
           }
         }
       });
@@ -57,52 +57,52 @@ function getTweets() {
 }
 
 function spotifySong() {
-  var Spotify = require('node-spotify-api');
+  var Spotify = require("node-spotify-api");
 
   var searchQuery = process.argv[3];
 
   var Spot = new Spotify({id: keys.spotifyKeys.client_id, secret: keys.spotifyKeys.client_secret});
 
   Spot.search({
-    type: 'track',
+    type: "track",
     query: searchQuery,
     limit: 5
   }, function(err, data) {
     if (err) {
       return console.log(err);
     }
-    console.log('Top Result');
-    console.log('---------------');
-    console.log('Song title: ' + JSON.stringify(data.tracks.items[0].name, null, 2));
-    console.log('Artist: ' + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
-    console.log('Album: ' + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-    console.log('Song Link: ' + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify, null, 2));
+    console.log("Top Result");
+    console.log("---------------");
+    console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+    console.log("Artist: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+    console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+    console.log("Song Link: " + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify, null, 2));
   });
 }
 
 function getMovie() {
   var movie = process.argv[3];
 
-  var queryUrl = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=40e9cece';
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
   request(queryUrl, function(response, body) {
     var content = JSON.parse(body);
     var tomatoes = JSON.stringify(content.Ratings[1].Value, null, 2);
 
     var values = [
-      'Title: ' + content.Title,
-      'Year: ' + content.Year,
-      'iMDB Rating: ' + content.imdbRating,
-      'Rotten Tomatoes Rating: ' + tomatoes,
-      'Country: ' + content.Country,
-      'Language: ' + content.Language,
-      'Actors: ' + content.Actors,
-      'Plot: ' + content.Plot
+      "Title: " + content.Title,
+      "Year: " + content.Year,
+      "iMDB Rating: " + content.imdbRating,
+      "Rotten Tomatoes Rating: " + tomatoes,
+      "Country: " + content.Country,
+      "Language: " + content.Language,
+      "Actors: " + content.Actors,
+      "Plot: " + content.Plot
     ];
 
     for (var i = 0; i < values.length; i++) {
       console.log(values[i]);
-      console.log('---------------');
+      console.log("---------------");
     }
   });
 }
@@ -127,39 +127,39 @@ function getMovie() {
 // }
 
 function bonJovi() {
-  var fs = require('fs');
+  var fs = require("fs");
 
-  fs.readFile('lyrics.txt', 'utf8', function(error, data) {
+  fs.readFile("lyrics.txt", "utf8", function(error, data) {
     if (error) {
       return console.log(error);
     }
 
-    var lyrics = data.split(',');
+    var lyrics = data.split(",");
 
-    var inquirer = require('inquirer');
+    var inquirer = require("inquirer");
 
     inquirer.prompt([
       {
-        type: 'list',
-        choices: ['man', 'woman', 'cowboy'],
-        message: 'I am a ',
-        name: 'cowboy'
+        type: "list",
+        choices: ["man", "woman", "cowboy"],
+        message: "I'm a ",
+        name: "cowboy"
       }, {
-        type: 'list',
-        choices: ['skateboard', 'carpet', 'a steel horse'],
-        message: 'on what do you ride?',
-        name: 'horse'
+        type: "list",
+        choices: ["skateboard", "carpet", "a steel horse"],
+        message: "on what do you ride?",
+        name: "horse"
       }, {
-        type: 'list',
-        choices: ['dead or alive'],
-        message: 'I am wanted...',
-        name: 'wanted'
+        type: "list",
+        choices: ["dead or alive"],
+        message: "I'm wanted...",
+        name: "wanted"
       }
     ]).then(function(inquirerResponse) {
-      if (inquirerResponse.cowboy === 'cowboy' && inquirerResponse.horse === 'a steel horse') {
+      if (inquirerResponse.cowboy === "cowboy" && inquirerResponse.horse === "a steel horse") {
         console.log(JSON.stringify(lyrics, null, 2));
       } else {
-        console.log('Try again');
+        console.log("Try again");
       }
     });
   });
@@ -168,16 +168,16 @@ function bonJovi() {
 // function calls
 
 function commands() {
-  if (command === 'tweets-of') {
+  if (command === "tweets-of") {
     getTweets();
-  } else if (command === 'spotify-this') {
+  } else if (command === "spotify-this") {
     spotifySong();
-  } else if (command === 'movie-this') {
+  } else if (command === "movie-this") {
     getMovie();
-  } else if (command === 'do-what-it-says') {
+  } else if (command === "do-what-it-says") {
     // doWhatItSays();
-    console.log('This feature is not enabled.');
-  } else if (command === 'bon-jovi') {
+    console.log("This feature is not enabled.");
+  } else if (command === "bon-jovi") {
     bonJovi();
   }
 }
